8da84b1573cc269f805d909ddc67a2d1ecbddcfb
wncf <2630473215@qq.com>
2022-06-18 09:50:56 +0800

    书签测试

---
+++ docs/.vuepress/components/books.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <div v-for="item of books" :key="item.title">
+      <h2 :id="item.title">
+        <a :href="`#${item.title}`" class="header-anchor">#</a>
+        {{ item.title }}
+      </h2>
+      <div class="books-fill">
+        <a class="item" :href="children.url" target="_blank" v-for="children of item.children" :key="children.title">
+          <div class="img-fill">
+            <img :src="`${children.url}/favicon.ico`" alt="" />
+          </div>
+          <div class="right">
+            <div class="title">{{ children.name }}</div>
+            <div class="value">{{ children.value }}</div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import xxbooks from "./books/日常.json";
+export default {
+  props: {
+    title: {
+      type: String,
+      required: false,
+      value: "",
+    },
+  },
+  data() {
+    return {
+      count: 1,
+    };
+  },
+  computed: {
+    books: () => {
+      return xxbooks;
+    },
+  },
+  methods: {
+    add() {
+      this.count++;
+    },
+  },
+};
+</script>
+<style>
+.books-fill {
+  padding: 10px 0;
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+}
+.item {
+  display: flex;
+  min-width: 184px;
+  width: 184px;
+  height: 40px;
+  padding: 10px 10px;
+  border-radius: 4px;
+  border: 1px solid transparent;
+  transition: all 0.2s ease-out;
+  cursor: pointer;
+  text-decoration: none !important;
+  line-height: 100%;
+}
+.item > .img-fill {
+  height: 40px;
+  width: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.item img {
+  display: block;
+  height: 24px;
+  width: 24px;
+}
+.item > .right {
+  margin-left: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.item > .right .title {
+  font-size: 16px;
+}
+.item > .right .value {
+  font-size: 12px;
+  color: #909090;
+}
+.item:hover {
+  border: 1px solid #f5f5f5;
+  box-shadow: 0 0 8px #ccc;
+}
+</style>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,6 +15,10 @@ module.exports = {
     nav: [
       { text: '首页', link: '/' },
       {
+        text: '书签',
+        link: '/pages/a6ff0d/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+      },
+      {
         text: '前端',
         link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
         items: [
